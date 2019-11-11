b6ab2bd38c8d806e7d664f3f9e10d805ba9858bd
icoblr <icoblr@icoblrs-MacBook-Pro.local>
2019-11-11 14:29:16 +0530

    initial change

--- server/routes.ts
+++ server/routes.ts
@@ -593,7 +593,7 @@ export class Router {
     );
 
     app.use(
-      "/contentPlayer/preview",
+      "/contentPlayer/preview/*",
       proxy(proxyUrl, {
         proxyReqPathResolver: function(req) {
           logger.debug(
